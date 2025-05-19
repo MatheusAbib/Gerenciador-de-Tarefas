5faf972b73664db531e5a4f7c51a16677a32f2eb
MATHEUS BILITARDO ABIB <97857@pmogi.local>
2025-05-19 12:13:57 -0300

    Mensagem descritiva explicando o que foi alterado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000',
+  //apiUrl: 'http://localhost:3000',
+  apiUrl: 'https://gerenciador-de-tarefas-1.onrender.com:51486'
 };
